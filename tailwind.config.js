0800d501fabfabaf2873f8328545495097c83115
neducod <nedunony16@gmail.com>
2026-01-01 21:15:47 +0100

    More errors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx,html}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          wine: "#722F37",
+        },
+      },
+    },
+  }
+  
