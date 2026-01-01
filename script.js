08c128ff41c4185015f78df32ccbe02941336fd5
neducod <nedunony16@gmail.com>
2026-01-02 00:55:05 +0100

    Javascript Feature Done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+
+function filterDiv(category) {
+    const containers = document.querySelectorAll(".containerr");
+
+    containers.forEach(container => {
+        if (container.dataset.category === category) {
+            container.style.display = "flex";
+        } else {
+            container.style.display = "none";
+        }
+    });
+}
+
+
+filterDivs('firstSlide');
